ab0f992a5f73a3ef0149d9125509c2ff11c20c3a
Paras Ladwa <154990057+parasladwa@users.noreply.github.com>
2023-12-27 17:17:47 +0000

    Add files via upload

---
+++ deck_of_cards.py
@@ -0,0 +1,149 @@
+import random
+
+
+#constants for creating deck
+VALUES = list(range(2, 15))
+
+SUITS = ["clubs", "diamonds", "hearts", "spades"]
+
+FACE_VALUES = {
+    "J": 11,
+    "Q": 12,
+    "K": 13,
+    "A": 14,
+    11: "J",
+    12: "Q",
+    13: "K",
+    14: "A"
+}
+
+
+
+
+
+#class of cards
+class Card:
+    
+    def __init__(self, value, suit):
+        self.value= value
+        self.suit = suit
+
+    
+
+
+#iterate through values and cards, creating deck
+def generate_cards(VALUES, SUITS):
+    
+    cards = [] 
+    
+    for value in VALUES:
+        for suit in SUITS:
+            
+            #if card val has face, use dict
+            if value in FACE_VALUES:
+                
+                card_value = FACE_VALUES[value]
+                cards.append(Card(card_value, suit))
+                
+            else:
+                cards.append(Card(value, suit))
+    
+    return cards
+
+
+
+
+def shuffle(deck):
+    
+    #new deck to add cards in random order
+    shuffled_deck = []
+    
+    while len(shuffled_deck) != 52:
+        
+        #select random card
+        
+        random_card = random.choice(deck)
+        
+        if random_card not in shuffled_deck:
+            
+            #add to new deck
+            shuffled_deck.append(random_card)
+            
+            #remove selected card from deck
+            deck.remove(random_card)
+        
+    return shuffled_deck
+       
+
+
+
+def main():
+    
+    ordered_deck = generate_cards(VALUES, SUITS)
+    
+    deck = shuffle(ordered_deck)
+
+    for card in deck:
+        print(card.value, card.suit)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
